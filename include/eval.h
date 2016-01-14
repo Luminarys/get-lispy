2f4595cddd4951f0113120df9bb7eac52067417f
Ezechiel Ethan Sealover <redrifle@cock.li>
2016-01-13 23:50:51 -0500

    removed stuff

--- include/eval.h
+++
@@ -1,7 +0,0 @@
-#ifndef _GL_EVAl_H
-#define _GL_EVAl_H
-
-lval* lval_eval(lenv* e, lval* v);
-lval* lval_eval_sexpr(lenv* e, lval* v);
-
-#endif
--- include/print.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GL_PRINT_H
-#define _GL_PRINT_H
-
-void lval_expr_print(lval* v, char open, char close);
-void lval_print(lval* v);
-void lval_print_str(lval* v);
-void lval_println(lval* v);
-
-#endif
